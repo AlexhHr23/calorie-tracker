2b0d8bf7f7d36e7af3d9c5f319c195336b47ec3a
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-04-06 13:06:12 -0600

    Almacenando id

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,15 +1,17 @@
 
+import { useMemo, Dispatch} from "react"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
-import { useMemo } from "react"
 import { PencilSquareIcon } from '@heroicons/react/24/outline'
+import { ActivityActions } from "../reducers/activityReducer"
 
 type ActivityListProps = {
-    activities: Activity[]
+    activities: Activity[],
+    dispatch: Dispatch<ActivityActions>
 }
 
 
-export const ActivityList = ({activities}: ActivityListProps) => {
+export const ActivityList = ({activities, dispatch}: ActivityListProps) => {
 
     const categoryName = useMemo(() => 
         (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
@@ -34,7 +36,9 @@ export const ActivityList = ({activities}: ActivityListProps) => {
                 </div>
 
                 <div className="flex gap-5 items-center"> 
-                    <button>
+                    <button
+                     onClick={() => dispatch({type:"set-activeId", payload: {id: activity.id}})}
+                    >
                         <PencilSquareIcon
                             className="h-8 w-8 text-gray-800"
                         />
