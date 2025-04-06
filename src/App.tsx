3fda84b35a391df2185e09fa14e9a283196ed95d
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-04-06 14:54:50 -0600

    Componente para cantidaes

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
     <section className="bg-gray-800 py-10">
       <div className="max-w-4xl mx-auto">
         <CalorieTracker
-          state={state.activities}
+          activities={state.activities}
         />
       </div>
     </section>
--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,12 +1,36 @@
+import { useMemo } from "react"
 import type { Activity } from "../types"
+import { CalorieDisplay } from "./CalorieDisplay"
 
 
 type CalorieTrackerProps = {
-    state: Activity[]
+    activities: Activity[]
 }
 
-export const CalorieTracker = ({state}: CalorieTrackerProps) => {
+export const CalorieTracker = ({activities}: CalorieTrackerProps) => {
+
+    //CONTADORES
+    const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? 
+    total + activity.calories : total, 0) ,[activities])
+
+    const caloriesTrained = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? 
+    total + activity.calories : total, 0) ,[activities])
+
   return (
-    <div>CalorieTracker</div>
+    <>
+        <h2 className="text-4xl font-black text-white text-center">Resumen de calorias</h2>
+        
+
+        <div className="flex flex-col items-center md:flex-row md: justify-between gap-5 mt-10">
+           <CalorieDisplay
+            calories={caloriesConsumed}
+            text='Cosmidas'
+           />
+           <CalorieDisplay
+            calories={caloriesTrained}
+            text='Ejercicio'
+           />
+        </div>
+    </>
   )
 }
