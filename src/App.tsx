4e34f22248ba72fb604a601879ec0a1d97a39ceb
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-04-06 14:38:37 -0600

    Componente calorie tracker

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect, useMemo } from "react"
 import { Form } from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activityReducer"
 import { ActivityList } from "./components/ActivityList"
+import { CalorieTracker } from "./components/CalorieTracker"
 
 
 
@@ -42,6 +43,14 @@ function App() {
           />
         </div>
       </section>
+
+    <section className="bg-gray-800 py-10">
+      <div className="max-w-4xl mx-auto">
+        <CalorieTracker
+          state={state.activities}
+        />
+      </div>
+    </section>
 
       <section className="p-10 mx-auto max-w-4xl">
         <ActivityList
---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,12 @@
+import type { Activity } from "../types"
+
+
+type CalorieTrackerProps = {
+    state: Activity[]
+}
+
+export const CalorieTracker = ({state}: CalorieTrackerProps) => {
+  return (
+    <div>CalorieTracker</div>
+  )
+}
